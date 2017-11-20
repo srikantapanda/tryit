f9f99fe8aa7df829e832f3ca95437f6ed16a7169
Srikanta Panda03 <Srikanta_Panda03@ad.infosys.com>
2017-11-20 22:30:26 +0000

    fix

--- demo/demo/src/main/java/com/example/demo/controller/WelcomeController.java
+++ demo/demo/src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 	
+	private String message = "Welcome to Demo Application";
+	
 	@RequestMapping("/welcome")
-	public String displayWelcomePage() {
+	public String displayWelcomePage(ModelMap model) {
+		model.addAttribute("welcome", "");
 		return "welcome";
 	}
+	
+	
 	
 }

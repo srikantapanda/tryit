1a3a021fa111bf823a5d24e508be3b553b0150e2
Srikanta Panda03 <Srikanta_Panda03@ad.infosys.com>
2017-11-20 20:42:27 +0000

    fix

--- demo/demo/src/main/java/com/example/demo/controller/WelcomeController.java
+++ demo/demo/src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TestController {
+public class WelcomeController {
 	
+	@RequestMapping("/welcome")
+	public String displayWelcomePage() {
+		return "welcome";
+	}
 	
-
 }
